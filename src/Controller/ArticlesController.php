ced8dc3d1a986aa3b6507abd6c592bb9fabf3448
Jason Stollman <Jason.Stollman@gmail.com>
2018-04-30 15:23:23 -0500

    Core package with Articles log

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\Controller;
+
+use Cake\Utility\Text;
+
+class ArticlesController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+    public function index()
+    {
+        $articles = $this->Paginator->paginate(
+            $this->Articles->find()
+        );
+        $this->set(compact('articles'));
+    }
+
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
+
+    public function create()
+    {
+        $article = $this->Articles->newEntity();
+        if($this->request->is('post')){
+            $article = $this->Articles->patchEntity(
+                $article,
+                $this->request->getData()
+            );
+
+            //$article->user_id = 1;
+
+
+
+            if($this->Articles->save($article)){
+                $this->Flash->success('Your article has been save');
+                return $this->redirect(['action'=>'index']);
+            }
+            $this->Flash->error('Unable to add your article');
+
+        }
+
+        $this->set(compact('article'));
+    }
+
+    public function edit($id = null)
+    {
+        $article = $this->Articles->findById($id)->firstOrFail();
+        if($this->request->is(['post', 'put'])){
+            $article = $this->Articles->patchEntity($article,$this->request->getdata()
+            );
+
+            if($this->Articles->save($article)){
+                $this->Flash->success(
+                    'Your article has been updated.'
+                );
+
+                return $this->redirect(['action'=>'index']);
+            }
+            $this->error('An error has occured.');
+        }
+        $this->set('article', $article);
+    }
+
+    public function delete($id=null){
+        $this->request->allowMethod(['post', 'delete']);
+
+        $article = $this->Articles->findById($id)->firstOrFail();
+        if($this->Articles->delete($article)){
+            $this->Flash->success(
+                "The article {$article->title} has been deleted."
+            );
+            return $this->redirect(['action'=>'index']);
+        }
+
+        $this->Flash->success(
+            "Something bad happened."
+        );
+    }
+}
+
+
